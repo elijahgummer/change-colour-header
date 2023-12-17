89319832a1911c9b95118c285dde5edc9b34eaec
Elijah W Gummer <96103526+elijahgummer@users.noreply.github.com>
2023-12-17 10:31:03 +1000

    Add files via upload

---
+++ header-color-changer/script.js
@@ -0,0 +1,14 @@
+const redButton = document.getElementById("red");
+const blueButton = document.getElementById("blue");
+const greenButton = document.getElementById("green");
+const Header = document.getElementById("header");
+
+function changeHeaderRed() {
+    Header.style.color = "red";
+}
+function changeHeaderGreen() {
+    Header.style.color = "green";
+}
+function changeHeaderBlue() {
+    Header.style.color = "blue";
+}
